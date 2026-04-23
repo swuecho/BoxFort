bd35d189f40148740a3592e4dbebc14bf56864b9
Hao Wu <echowuhao@gmail.com>
2026-04-23 20:18:43 +0800

    fix: add missing include and fix off-by-one error in shim array

--- src/filc-shims.c
+++ src/filc-shims.c
@@ -5,7 +5,14 @@
 
 #include <link.h>
 
-int pizlonated_dl_iterate_phdr(int (*callback)(struct dl_phdr_info *info, size_t size, void *data), void *data)
+/*
+ * Force the exported symbol name to exactly `pizlonated_dl_iterate_phdr`.
+ * Fil-C may otherwise apply its own renaming to this definition.
+ */
+int bxfi_filc_dl_iterate_phdr(int (*callback)(struct dl_phdr_info *info, size_t size, void *data), void *data)
+    __asm__("pizlonated_dl_iterate_phdr");
+
+int bxfi_filc_dl_iterate_phdr(int (*callback)(struct dl_phdr_info *info, size_t size, void *data), void *data)
 {
     return dl_iterate_phdr(callback, data);
 }
